26dcc56211e45c2ad59741ed6c31eca4c022fd2b
Dariusz Mydlarz <dmydlarz@gmail.com>
2013-12-11 18:24:13 +0100

    Przepisanie parsera od nowa, prostszy kod, itd

--- src/parser-pap.py
+++ src/parser-pap.py
@@ -2,81 +2,59 @@
 # -*- coding: utf-8 -*-
 
 from plp import PLP
-import fileinput
-import re
 import sys
+import fileinput
 
 p = PLP()
+lineSeparator = '#'
 
-separator = r'#\d+'
-separatorPattern = re.compile(separator)
+def main(bodziec):
+	forms = getForms(bodziec)
+	paragraphs = getParagraphs(forms)
+	printParagraphs(paragraphs)
 
-bodziecWord = None
+def getForms(bodziec):
+	return map(lambda x : p.forms(x), p.orec(bodziec))[0]
 
-inputLines = []
-containingLines = []
-    
-def main():
-    global inputLines
-    forms = getForms()
-    # print(forms)
-    inputLines = readInput()
-    parseLines(forms)
-    paragraphs = sorted(getParagraphs())
-    # print("Found", len(paragraphs), "paragraphs with given word")
-    printParagraphs(paragraphs)
-    
+def getParagraphs(forms):
+	paragraphs, p = [], []
+	containsStimulant = False
+	for line in fileinput.input(sys.argv[2]):
+		line = line.strip().decode("utf-8")
+		# end of paragraph
+		if not line or lineSeparator in line:
+			if containsStimulant:
+				paragraphs.append(p)
+			containsStimulant = False
+			p = []
+
+		if any(form in line for form in forms):
+			containsStimulant = True
+		p.append(line)
+
+	# flushing left lines
+	if p and containsStimulant:
+		paragraphs.append(p)
+	return paragraphs
+
+def hasForm(forms, line):
+	for form in forms:
+		if form in line: return True
+	return False
+
 def printParagraphs(paragraphs):
-    for begin, end in paragraphs:
-        for i in range(begin, end):
-            print(inputLines[i]),
-        print("")
-
-def parseLines(forms):
-    for i, line in enumerate(inputLines):
-        for form in forms:
-            if form in line:
-                containingLines.append(i)
-                break
-
-def getForms():
-    return map(lambda x : p.forms(x), p.orec(bodziecWord))[0]
-
-def getParagraphs():
-    paragraphs = set()
-    for lineNumber in containingLines:
-        paragraphs.add(getParagraph(lineNumber))
-    return paragraphs
-
-def getParagraph(lineNumber):
-    begin = getParagraphBegin(lineNumber)
-    end = getParagraphEnd(lineNumber)
-    return begin, end
-
-def getParagraphBegin(lineNumber):
-    for line in range(lineNumber, 0, -1):
-        if(separatorPattern.match(inputLines[line])):
-            return line
-    return 0
-
-def getParagraphEnd(lineNumber):
-    length = len(inputLines)
-    for line in range(lineNumber, length):
-        if(separatorPattern.match(inputLines[line])):
-            return line
-    return 0
-
-def readInput():
-    inputLines = []
-    for line in fileinput.input(sys.argv[2]):
-        inputLines.append(line.decode("utf-8"))
-    return inputLines
+	# print(len(paragraphs))
+	for p in paragraphs:
+		for line in p: 
+			print(line)
+		print("")
 
 if __name__ == "__main__":
     try:
         if len(sys.argv) < 3:
-            sys.exit("Usage: %s <bodziec-word> <parseFile>" % sys.argv[0])
-        bodziecWord = sys.argv[1].decode("utf-8")
-        main()
+            sys.exit("Usage: %s <bodziec-word> <file-to-parse>" % sys.argv[0])
+    	reload(sys)
+    	sys.setdefaultencoding("utf-8")
+        main(sys.argv[1].decode("utf-8"))
     except KeyboardInterrupt:
         print("Oh, man! There's no results yet... See you later!")    
